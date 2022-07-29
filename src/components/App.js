d92935201bc3e634c20b886381e18590ec3c22a8
GLADYS <mungaiglado106@gmail.com>
2022-07-29 13:05:47 +0300

    commit tree e905fcce42d072bc695f809808d2928588a84d9f

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,27 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
 
+
+// child component
+
+function MovieCard({ 
+  title,
+  genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+
+  }) {
+
+  return (
+    <div className="movie-card">
+      <img src={posterSrc} alt={title} />
+      <h2>{title}</h2>
+      <small>{genres.join(",")}</small>
+    </div>
+  );
+}
 export default App;
